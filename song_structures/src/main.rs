45f7c75191ab98ab3ca7c866fe797d27c75f4c39
Garrett Hill <gahill2018@protonmail.com>
2021-11-18 11:18:46 -0500

    Updated dependencies, got basic async functionality working

--- song_structures/src/main.rs
+++ song_structures/src/main.rs
@@ -1,14 +1,31 @@
 use genius_rs::Genius;
+//use threadpool::ThreadPool;
+use mini_redis::{client, Result};
+//use std::future::Future;
+//use futures::executor::LocalPool;
+//use std::pin::Pin;
 
-async fn scrape_artist(artist: &str) -> &str{
+
+async fn scrape_artist(artist: &str) {
     let token = "tu4mFXq-j8GlG9mqZQlEBb0yeekm_zC5A3-mt2RxYVF3qTAPrLybFl_ykJ7Fk_E5-dyQ7bMCXHNSbaYjCOQ47g";
     let genius = Genius::new(token.to_string());
-    let response = genius.search(artist).await.unwrap();
-    println!("{}", response[0].result.full_title);
-    response
+
+    println!("test2");
+
+    match genius.search(artist).await {
+        Ok(response) => println!("{:?}", response[0].result.full_title.clone()),
+        _ => println!("genius query failed"),
+
+    }
 }
 
-fn main() {
-    let results = scrape_artist("Ariana Grande");
+#[tokio::main]
+async fn main() -> Result <()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
 
+    let artist_results = scrape_artist("test");
+
+    artist_results.await;
+
+    return Ok(());
 }
