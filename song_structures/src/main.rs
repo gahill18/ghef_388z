ac6be1be3d1d1edf13b9bf0654a7717d6653edaf
Garrett Hill <gahill2018@protonmail.com>
2021-11-20 16:49:50 -0500

    Simplified calls, currently diagnosing lack of returned lyrics

--- song_structures/src/main.rs
+++ song_structures/src/main.rs
@@ -3,40 +3,31 @@ use mini_redis::Result;
 use std::collections::HashMap;
 use std::env;
 
-// For given artist, returns HashMap where song titles are mapped to their genius url's
-async fn scrape_song_urls_for_artist(artist: &str) -> HashMap<String, String> {
-    let token = "iD4KBNaCN84dVIvRTsT7aUZS2ZvJbXEqI0CfARtb96RfsEAbliPg0ZdW1ObFCLb7";
+// For given artist, returns HashMap where song titles are mapped to their genius page's lyrics
+async fn scrape_song_lyrics_for_artist(artist: &str) -> HashMap<String, Vec<String>> {
+    let token = "hfCV-emITedExDDPNttLW6pWtM2IEJYD1VbVe5E3g4x6-7kr8A2NSRZaAIqe3WUz";
     let genius = Genius::new(token.to_string());
-    let mut songs: HashMap<String, String> = HashMap::new();
+    let mut songs: HashMap<String, Vec<String>> = HashMap::new();
 
     match genius.search(artist).await {
         Ok(response) => {
             for n in 0..response.len() {
-                songs.insert(response[n].result.full_title.clone(), response[n].result.url.clone());
+                match genius.get_lyrics(&response[n].result.url).await {
+                    Ok(lyrics) => {
+                        let title = response[n].result.full_title.clone();
+                        songs.insert(title, lyrics);
+                    },
+                    e => println!("genius song lyrics query failed, returned {:?}", e),
+                }
             }
         },
         e => println!("genius song urls query failed, returned {:?}", e),
 
     }
 
+    println!("method run");
+
     songs
-}
-
-async fn get_lyrics_for_songs(songs: HashMap<String, String>) -> HashMap<String, Vec<String>> {
-    let token = "iD4KBNaCN84dVIvRTsT7aUZS2ZvJbXEqI0CfARtb96RfsEAbliPg0ZdW1ObFCLb7";
-    let genius = Genius::new(token.to_string());
-    let mut song_lyrics: HashMap<String, Vec<String>> = HashMap::new();
-
-    for (title, url) in songs.iter() {
-        match genius.get_lyrics(url).await {
-            Ok(response) => {
-                song_lyrics.insert(title.to_string(), response.clone());
-            },
-            e => println!("genius song lyrics query failed, returned {:?}", e),
-        }
-    }
-
-    song_lyrics
 }
 
 #[tokio::main]
@@ -45,16 +36,11 @@ async fn main() -> Result <()> {
     println!("args: {:?}", args);
 
     let artist = &args[1];
+    let lyrics = scrape_song_lyrics_for_artist(artist);
 
-    let songs = scrape_song_urls_for_artist(artist);
-    let songs = songs.await;
-
-    println!("For artist {:?}, found songs {:?}", artist, songs);
-
-    let lyrics = get_lyrics_for_songs(songs);
     let lyrics = lyrics.await;
 
-    println!("Lyrics: {:?}", lyrics);
+    println!("lyrics: {:?}", lyrics);
 
     return Ok(());
 }
