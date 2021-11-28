bf3866ac6bf8915db4edf28f832af305a50082d5
Garrett Hill <gahill2018@protonmail.com>
2021-11-28 17:58:15 -0500

    Lyrics are now being isolated from the page

--- song_structures/src/main.rs
+++ song_structures/src/main.rs
@@ -1,11 +1,16 @@
+extern crate reqwest;
+extern crate select;
+
+use scraper::{Html, Selector};
+
 use genius_rs::Genius;
-extern crate reqwest; 
 use mini_redis::Result;
+
 use std::collections::HashMap;
 use std::env;
 
 // For given artist, returns HashMap where song titles are mapped to their linked genius page's contents
-async fn scrape_pages_for_artist(artist: &str) -> HashMap<String, String> {
+async fn get_urls_for_artist(artist: &str) -> HashMap<String, String> {
     let token = "hfCV-emITedExDDPNttLW6pWtM2IEJYD1VbVe5E3g4x6-7kr8A2NSRZaAIqe3WUz";
     let genius = Genius::new(token.to_string());
     let mut songs: HashMap<String, String> = HashMap::new();
@@ -15,41 +20,41 @@ async fn scrape_pages_for_artist(artist: &str) -> HashMap<String, String> {
         Ok(response) => {
             // For every response, get the page at the url provided to parse
             for n in 0..response.len() {
-                match reqwest::get(&response[n].result.url).await {
-                    Ok(page) => {
-                        match page.text().await {
-                            Ok(lyrics) => {
-                                let title = response[n].result.full_title.clone();
-                                songs.insert(title, lyrics);
-                            },
-                            e => println!("genius page text query failed, returned {:?}", e),
-                        };
-                    },
-                    e => println!("genius song url query failed, returned {:?}", e),
-                }
+                let title = response[n].result.full_title.clone();
+                songs.insert(title, response[n].result.url.clone());
             }
         },
-        e => println!("genius song query failed, returned {:?}", e),
+        Err(e) => println!("genius song query failed, returned {:?}", e),
 
     }
-
-    println!("method run");
 
     songs
 }
 
-fn get_lyrics_from_page(page_contents: String) -> Vec<String> {
-    let mut output = Vec::new();
-    let mut processed_page = page_contents;
-    output.insert(0, processed_page);
-
-    output
-}
-
-fn get_lyrics_for_pages (map: &mut HashMap<String, String>) -> HashMap<String, Vec<String>>{
+async fn get_lyrics_for_url(url: &str) -> HashMap<String, Vec<String>> {
     let mut output: HashMap<String, Vec<String>> = HashMap::new();
-    for (song_title, page_contents) in &*map {
-        output.insert(song_title.to_string(), get_lyrics_from_page(page_contents.to_string()));
+
+    match reqwest::get(url).await {
+        Ok(response) => {
+            // Gets the HTML as a string
+            match response.text().await {
+                Ok(body) => {
+                    let fragment = Html::parse_document(&body);
+                    match Selector::parse("#lyrics-root") {
+                        Ok(selector) => {
+                            for lyrics in fragment.select(&selector) {
+                                let save_lyrics = lyrics.text().collect::<Vec<_>>();
+                                println!("Saved lyrics: {:?}", save_lyrics);
+                                println!("\n\n\n\n");
+                            }
+                        },
+                        Err(e) => println!("selector failed"),
+                    }
+                },
+                Err(e) => println!("fragment text failed"),
+            }
+        },
+        Err(e) => println!("url reqwest failed"),
     }
 
     output
@@ -58,16 +63,16 @@ fn get_lyrics_for_pages (map: &mut HashMap<String, String>) -> HashMap<String, V
 #[tokio::main]
 async fn main() -> Result <()> {
     let args: Vec<String> = env::args().collect();
-    println!("args: {:?}", args);
 
     let artist = &args[1];
-    let pages = scrape_pages_for_artist(artist);
+    let artist_urls = get_urls_for_artist(artist);
 
-    let mut pages = pages.await;
-    println!("pages: {:?}", pages);
+    let artist_urls = artist_urls.await;
+    println!("artist_urls: {:?}\n\n\n\n", artist_urls);
 
-    let lyrics = get_lyrics_for_pages(&mut pages);
-    println!("lyrics: {:?}", lyrics);
+    for (_title, url) in artist_urls {
+        get_lyrics_for_url(&url).await;
+    }
 
     return Ok(());
 }
