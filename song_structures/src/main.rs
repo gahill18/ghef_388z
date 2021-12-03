e6491ff37fe91961a0f9bfd15485a2d7bebea757
cirecirecire <eafields2010@gmail.com>
2021-12-02 21:59:18 -0500

    updated src/main.rs so that it creates a directory called src/lyrics and stores artists songs in .txt files in a subdirectory of src/lyrics corresponding to the artist name

--- song_structures/src/main.rs
+++ song_structures/src/main.rs
@@ -8,6 +8,9 @@ use mini_redis::Result;
 
 use std::collections::HashMap;
 use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
 
 // For given artist, returns HashMap where song titles are mapped to their linked genius page's contents
 async fn get_urls_for_artist(artist: &str) -> HashMap<String, String> {
@@ -33,7 +36,7 @@ async fn get_urls_for_artist(artist: &str) -> HashMap<String, String> {
 
 // Takes in a genius url and a write path, gets the lyrics from the url,
 // then writes to the file at write_location
-async fn get_lyrics_for_urls(url: &str, write_location: &str) {
+async fn write_lyrics_from_urls(url: &str, song_title: &str, artist: &str) {
     match reqwest::get(url).await {
         Ok(response) => {
             // Gets the HTML as a string
@@ -44,8 +47,26 @@ async fn get_lyrics_for_urls(url: &str, write_location: &str) {
                         Ok(selector) => {
                             for lyrics in fragment.select(&selector) {
                                 let save_lyrics = lyrics.text().collect::<Vec<_>>();
+                                
+                                let mut filename = "./lyrics/".to_owned();
+                                filename.push_str(artist);
+                                filename.push_str("/");
+                                filename.push_str(song_title);
+                                filename.push_str(".txt");
+                                
+                                //fs::write(filename, "hello world").expect("Unable to write file");
                                 println!("lyrics before writing: {:?}\n\n", save_lyrics); // modify this line for file I/O
+                                let data = "Hello world1";
+                                let mut f = File::create(filename).expect("Unable to create file");
+                                for line in &save_lyrics[0..save_lyrics.len() - 6] { // - 6 is for the last 6 parts of lyrics page that aren't actually lyrics
+
+                                    f.write_all(line.as_bytes()).expect("Unable to write line");
+                                    f.write_all("\n".as_bytes()).expect("Unable to write new line")
+                                }
+                                
+
                             }
+                            
                         },
                         Err(e) => println!("selector failed"),
                     }
@@ -59,17 +80,27 @@ async fn get_lyrics_for_urls(url: &str, write_location: &str) {
 
 #[tokio::main]
 async fn main() -> Result <()> {
+    fs::create_dir("./lyrics");
     let args: Vec<String> = env::args().collect();
 
-    let artist = &args[1];
-    let artist_urls = get_urls_for_artist(artist);
+    let artist = &args[1].to_lowercase().replace(" ", "_");
+    let artist_urls = get_urls_for_artist(&artist);
 
     let artist_urls = artist_urls.await;
     println!("artist_urls: {:?}\n\n\n\n", artist_urls);
 
-    let write_path = &args[2];
-    for (_title, url) in artist_urls {
-        get_lyrics_for_urls(&url, write_path).await;
+    // let write_path = &args[2];
+
+    for (raw_title, url) in artist_urls {
+        let mut filename = "./lyrics/".to_owned();
+        filename.push_str(&artist);
+        fs::create_dir(filename);
+        // title will be the song title, which is ended by the word "by\u{a0}" in the url which is used for extracting it
+        let title = &(raw_title[0..raw_title.find("by\u{a0}").unwrap() - 1].to_lowercase().replace(" ", "_")); // changes spaces in the song title to be _
+        // print!("title was: {:?}", title);
+        // print!("raw_title was: {:?}", raw_title);
+        write_lyrics_from_urls(&url, title, artist).await;
+        
     }
 
     return Ok(());
