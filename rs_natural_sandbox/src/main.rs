849ad0a2cad89f4d047856b3747f8a3072635170
Garrett Hill <gahill2018@protonmail.com>
2021-12-12 20:37:08 -0500

    Polishing interaction between song_structures and rs_natural_sandbox

--- rs_natural_sandbox/src/main.rs
+++ rs_natural_sandbox/src/main.rs
@@ -3,37 +3,58 @@ extern crate natural;
 use natural::classifier::NaiveBayesClassifier;
 
 use std::collections::HashMap;
-use std::env;
-use std::fs;
+//use std::error::Error;
+use std::{env, fs};
 use std::fs::File;
 use std::io::prelude::*;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-     if args.len() != 2 {
-        println!("proper usage: cargo run [path_to_dataset]");
+     if args.len() != 3 {
+        println!("proper usage: cargo run [mode] [path_to_dataset]");
+        println!("mode can be either \"raw\" or \"prefab\"");
         return;
     }
 
-    
-    
+    let mode = args.get(1).unwrap();
+    println!("mode: {:?}", mode);
 
-    let path_to_dataset = args.get(1).unwrap();
-    println!("path_to_dataset: {:?}", path_to_dataset);
-    
-    let dataset = generate_dataset(path_to_dataset);
-    let mut nbc = NaiveBayesClassifier::new();
-    
-    train_on_dataset(dataset, &mut nbc);
+    let path_to_dataset = args.get(2).unwrap();
 
-    guess_for("strand_kiss", &mut nbc);
-    guess_for("handsome valid", &mut nbc);
+    if mode.eq("raw") {
+        match lyrics_to_key_label(path_to_dataset) {
+            Ok(new_path) => {
+                let dataset = generate_dataset(new_path, "\n");
+                let mut nbc = NaiveBayesClassifier::new();
+        
+                train_on_dataset(dataset, &mut nbc);
+                guess_for("raw mode", &mut nbc);
+            },
+            Err(text) => {
+                println!("{:?}", text);
+            },
+        }
+
+    } 
+
+    else if mode.eq("prefab") {
+        let dataset = generate_dataset(&path_to_dataset, ",");
+        let mut nbc = NaiveBayesClassifier::new();
+
+        train_on_dataset(dataset, &mut nbc);
+        guess_for("prefab mode", &mut nbc);
+    } 
+
+    else {
+        println!("non-valid mode input");
+        return
+    }
 }
 
 // Generating Dataset Section
 
-fn generate_dataset (path_to_dataset: &str) -> HashMap<String, String> {
+fn generate_dataset (path_to_dataset: &str, split_on: &str) -> HashMap<String, String> {
     let mut dataset = HashMap::new();
 
     match File::open(path_to_dataset) {
@@ -41,12 +62,12 @@ fn generate_dataset (path_to_dataset: &str) -> HashMap<String, String> {
             // Get raw file contents
             let mut raw_file_contents = String::new();
             match file.read_to_string(&mut raw_file_contents) {
-                Ok(size) => println!("raw file contents: {:?}", raw_file_contents),
+                Ok(size) => println!("size: {:?}, raw file contents: {:?}", size, raw_file_contents),
                 Err(e) => println!("failed to read from file: {:?}", e),
             }
 
             // Get key label pairs
-            let key_label_pairs =raw_file_contents.split("\n");
+            let key_label_pairs =raw_file_contents.split(split_on);
             for key_label_pair in key_label_pairs {
                 let mut k_l_split = key_label_pair.split("/");
                 let k_to_unwrap = k_l_split.next();
@@ -81,7 +102,52 @@ fn train_on_dataset (dataset: HashMap<String, String>, nbc: &mut NaiveBayesClass
     }
 }
 
+// Simple wrapper function for debugging purposes
+// Takes in a string for the classifier to guess the label of
 fn guess_for (string_to_guess: &str, nbc: &mut NaiveBayesClassifier) {
     let guess = nbc.guess(string_to_guess);
     println!("nbc guessed {:?} fits label {:?}", string_to_guess, guess);
+}
+
+// Testing Helper Functions Section
+
+/*
+Takes in a path to the lyrics folder from song_structures output and creates a text file
+that can be read by the dataset generator
+*/
+fn lyrics_to_key_label(path_to_lyrics_folder: &str) -> Result<&str, &str> {
+    // If file to write to is successfully created
+    match fs::read_dir(path_to_lyrics_folder) {
+        Ok(paths) => {
+            // If the path to the lyrics folder is valid, collect all of the lyrics with their genres
+            let mut contents = String::new();
+            for path_result in paths {
+                match path_result {
+                    Ok(path) => {
+                        match File::open(path.path()) {
+
+                            Ok(mut file) => {
+                               let _result = file.read_to_string(&mut contents);
+                               contents.push_str("\temp genre,");
+                            },
+                            Err(e) => println!("error reading path: {:?}", e),
+                        }
+                    }
+                    Err(e) => println!("error reading path_result: {:?}", e),
+                }
+            }
+                
+            let path_to_new_database = "./bin/output.txt";
+            match fs::write(path_to_new_database, contents) {
+                Ok(_result) => {
+                    println!("successfully wrote contents to file");
+                    let output = path_to_new_database.clone();
+                    Ok(output)
+                },
+                Err(_e) => core::result::Result::Err("failed to write contents to file"),
+            }
+        },
+        Err(_e) => core::result::Result::Err("failed to write contents to file"),
+    }      
+    
 }
